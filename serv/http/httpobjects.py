4b8413039041a41fbbb0e657e21367e5c79d0671
Kent Ma <tnekent@gmail.com>
2018-03-04 22:23:53 +0800

    Working HTTP Server now

--- serv/http/httpobjects.py
+++ serv/http/httpobjects.py
@@ -1,23 +1,71 @@
 #!/usr/bin/python3
 class HTTPObject(object):
-    def __init__(self, ver):
-        self.version = ver
+    def __init__(self, version):
+        self.version = version
         self.headers = {}
+        self.data = ""
+    def export_headers(self):
+        data_len = len(self.data)
+        if data_len > 0:
+            self.headers["Content-Length"] = [str(data_len)]
+
+        header_resp = []
+
+        for header in self.headers:
+            header_val = "".join(self.headers[header])
+            header_resp.append("%s: %s\r\n" %(header, header_val))
+
+        header_resp.append("\r\n")
+        return "".join(header_resp) 
 
 
 class HTTPResponse(HTTPObject):
-    def __init__(self, ver, status_code, reason_phrase):
-        super().__init__(ver)
+    """
+
+    From the rfc2616:
+
+           After receiving and interpreting a request message, a server responds
+       with an HTTP response message.
+
+           Response      = Status-Line               ; Section 6.1
+                           *(( general-header        ; Section 4.5
+                            | response-header        ; Section 6.2
+                            | entity-header ) CRLF)  ; Section 7.1
+                           CRLF
+                           [ message-body ]          ; Section 7.2
+
+    """
+    def __init__(self, status_code, reason_phrase, version="HTTP/1.1", data=""):
+        super().__init__(version)
+        self.status_code = status_code
+        self.reason_phrase = reason_phrase
+        self.data = data
+
+    def status_line(self):
+        """
+            BNF is as follows: 
+                Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
+        """
+
+        return "%s %s %s\r\n" %(self.version, self.status_code, self.reason_phrase)
+
+
+    def serialize(self):
+        """
+            Build HTTP response string
+        """
+        resp = [self.status_line(), self.export_headers(), self.data, "\r\n"]
+        print(resp)
+        return "".join(resp)
 
 
 valid_methods = {"OPTIONS", "GET", "HEAD", "POST", "PUT", "DELETE", "TRACE", "CONNECT" }
 
 class HTTPRequest(HTTPObject):
-    def __init__(self, method, uri, ver):
-        super().__init__(ver)
+    def __init__(self, method, uri, version):
+        super().__init__(version)
         if method not in valid_methods:
             raise ValueError("Malformed HTTP Request Method")
 
         self.method = method
         self.uri = uri
-        self.data = ""
--- serv/http/listener.py
+++ serv/http/listener.py
@@ -1,11 +1,42 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import socket
+import sys
+import traceback
+
 from tokenizer import Tokenizer
+from httpobjects import *
+import parser
 
 class HTTPServ(object):
     def __init__(self):
-        pass
+        self.routes = {}
+
+    def add_handler(self, route, callback, methods={"GET"}):
+        self.routes[route] = (callback, methods)
+
+    def handle_connection(self, client, addr):
+        tokenizer = Tokenizer()
+        buf = client.recv(1024)
+
+        while tokenizer.tokenize_buf(buf):
+            buf = client.recv(1024)
+        
+        obj = parser.parse(tokenizer.export_tokens())
+
+        if "Content-Length" in obj.headers:
+            obj.data = client.recv(obj.headers["Content-Length"])
+
+        resp = self.call_handler(obj)
+        client.send(resp.serialize().encode("utf-8"))
+
+    def call_handler(self, req_obj):
+        if req_obj.uri in self.routes:
+            route = self.routes[req_obj.uri]
+            if req_obj.method in route[1]:
+                return route[0](req_obj)
+
+        return HTTPResponse(404, "Not Found", data="Not found")
 
     def listen_and_serve(self, serv_addr):
         host, port = serv_addr.split(":")
@@ -16,27 +47,23 @@ class HTTPServ(object):
         self.port = int(port)
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind((self.host, self.port))
-        sock.listen(5)
+        try:
+            sock.bind((self.host, self.port))
+            sock.listen(5)
 
-        while True:
-            tokenizer = Tokenizer()
-            client, addr = sock.accept()
+            while True:
+                client, addr = sock.accept()
+                self.handle_connection(client, addr)
 
-            buf = client.recv(1024)
-            while tokenizer.tokenize_buf(buf):
-                pass
-            
-            client.send("test")
-
-            for i in tokenizer.export_tokens():
-                print(i)
-        sock.close()
+        except Exception:
+            traceback.print_exc()
+        finally: 
+            sock.close()
 
 
 def test():
     serv = HTTPServ()
-    serv.listen_and_serve(":8080")
+    serv.listen_and_serve(":" + sys.argv[1])
 
 if __name__ == "__main__":
     test()
