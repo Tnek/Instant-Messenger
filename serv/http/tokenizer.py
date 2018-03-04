b52884f387fa7c2a79d6ae6aa1bdf17827ff4a34
Kent Ma <tnekent@gmail.com>
2018-03-04 22:30:28 +0800

    Removed test method

--- serv/http/tokenizer.py
+++ serv/http/tokenizer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from parser import *
 
 separators = { 
     ":":"COLON",
@@ -78,22 +77,3 @@ class Tokenizer(object):
     def export_tokens(self):
         return list(map(str, self.tokens))
 
-
-def test():
-    with open("tests/httpsample2", "r") as FILE:
-        a = FILE.read()
-    print(a)
-
-    t_test = Tokenizer()
-    for c in a:
-        if not t_test.add_char(c):
-            break
-    #print(t_test.export_tokens())
-    httpobj = parse(t_test.export_tokens())
-    print(httpobj.headers)
-
-    for i in httpobj.headers:
-        print("%s -> %s" %(i, httpobj.headers[i]))
-
-if __name__ == "__main__":
-    test()
