7914ec8a597dab835db16a2356642cb33c5122a0
Kent Ma <tnekent@gmail.com>
2018-03-04 22:40:14 +0800

    Added documentation fo rparser methods

--- serv/http/parser.py
+++ serv/http/parser.py
@@ -10,19 +10,20 @@ def parse(tokens):
         :param tokens: List of tokens to parse from
         :return: HTTPRequest object to fill
     """
-
     req = HTTPRequest(tokens[0], tokens[1], tokens[2])
     _parse_headers(req, tokens, 4)
 
     return req
-
     
 def _parse_headers(req_obj, tokens, cur):
     """
+        If a header is invalid, ignore it. Other HTTP servers respond an error 
+        for invalid headers - may change to return malformed response on invalid
+        header instead.
+
         :param req_obj: HTTPRequest object to fill
         :param tokens: List of tokens to parse from
         :param cur: Index of tokens where the statusline starts from
-        :return: Updated cur which is index after parsing
     """
     headers = []
     header_build = []
@@ -40,9 +41,7 @@ def _check_header(header):
         :param header: list of tokens corresponding to a header
         :return: Whether the header was valid or not
     """
-    if header[1] != ':': # Malformed header
+    if header[1] != ':':
         return False
     return True
-
-
 
