4704bddfe7d0ed3f4c692da65baed49f0cfbf275
Alice Lu <lalice72098@yahoo.com>
2018-03-31 16:49:01 +0800

    fix tnek css

--- static/js/messenger.js
+++ static/js/messenger.js
@@ -1,3 +1,4 @@
+// SideBarList ====================================================
 class SideBarList {
   constructor(input, target, list_entry_generator) {
     this.input = input;
@@ -18,28 +19,28 @@ class SideBarList {
 }
 
 function conv_barentry(item) {
-    return `<li class="clearfix user-li" onclick="messenger.select_conv('${item}')">
-          <div class="about">
-            <div class="name name-field">
+  return `<li class="clearfix user-li" onclick="messenger.select_conv('${item}')">
+            <div class="about">
+              <div class="name name-field">
                <span class="fa fa-comments online"></span>
                ${item.slice(1)}
-            </div>
-          </div> 
-        </li>`
+              </div>
+            </div> 
+          </li>`
 }
 
 function pm_barentry(item) {
-    return `<li class="clearfix user-li" onclick="messenger.select_conv('${item}')">
-          <div class="about">
-            <div class="name name-field">
-              <span class="fa fa-circle online"></span>
-              ${item}
-            </div>
-          </div> 
-        </li>`
+  return `<li class="clearfix user-li" onclick="messenger.select_conv('${item}')">
+            <div class="about">
+              <div class="name name-field">
+                <span class="fa fa-circle online"></span>
+                ${item}
+              </div>
+            </div> 
+          </li>`
 }
 
-
+// UserSelectModal ====================================================
 class UserSelectModal {
   constructor(input, target) {
     this.input = input;
@@ -56,11 +57,11 @@ class UserSelectModal {
     visible_items.map( item => {
       if (!this.selected_users.hasOwnProperty(item)) {
         $(this.target).append(`
-          <div class="list-group-item list-group-item-action" href="#" onclick="users_search.add_to_selected('${item}')">${item}</div>
+          <i class="list-group-item list-group-item-action" onclick="users_search.add_to_selected('${item}')">${item}</i>
         `);
       } else {
         $(this.target).append(`
-          <div class="list-group-item list-group-item-action active" href="#" onclick="users_search.remove_selected('${item}')">${item}</div>
+          <i class="list-group-item list-group-item-action active" onclick="users_search.remove_selected('${item}')">${item}</i>
         `);
       }
     });
@@ -80,85 +81,88 @@ class UserSelectModal {
   }
   make_group() {
     $.post("/newgroup", {
-        title: $("#titleForm").val(),
-        users: Object.keys(this.selected_users).join("&")
+      title: $("#titleForm").val(),
+      users: Object.keys(this.selected_users).join("&")
     });
     $('#newChatModal').modal('hide');
   }
 }
 
+var users_search = new UserSelectModal("#search-checklist", "#modal-list");
 
-var users_search = new UserSelectModal("#search-checklist", "#modal-listgroup");
-
+// Messenger ====================================================
 class Messenger {
-    constructor() {
-        this.selected_conversation = null;
-        this.get_conversations();
-        this.get_active_contacts();
-        this.conversations = [];
-        this.contacts = [];
+  constructor() {
+    this.selected_conversation = null;
+    this.get_conversations();
+    this.get_active_contacts();
+    this.conversations = [];
+    this.contacts = [];
 
-        this.conversations_bar = new SideBarList("#search-group", "#group-message-list", conv_barentry);
-        this.pm_bar = new SideBarList("#search-private", "#private-message-list", pm_barentry);
+    this.conv_bar = new SideBarList("#search-group", "#group-message-list", conv_barentry);
+    this.pm_bar = new SideBarList("#search-private", "#private-message-list", pm_barentry);
 
-        users_search = new UserSelectModal("#search-checklist", "#modal-listgroup");
-        this.tick();
+    users_search = new UserSelectModal("#search-checklist", "#modal-list");
+    this.tick();
+  }
+
+  get_active_contacts() {
+    $.getJSON("/users", contacts => {
+      this.contacts = contacts;
+    });
+    setTimeout(this.get_active_contacts, 1000);
+  }
+
+  get_conversations() {
+    $.getJSON("/conversations", convs => {
+      this.conversations = convs;
+    });
+  }
+
+  render_conversations() { 
+    this.conv_bar.render(this.conversations.map(values => values.title)); 
+  } 
+
+  render_pms() { this.pm_bar.render(this.contacts); } 
+
+  render_users_search() { users_search.render(this.contacts); }
+
+  render() {
+    this.render_conversations();
+    this.render_pms();
+    this.render_users_search();
+  }
+
+  select_conv(conv) {
+    if (this.conversations.map(values => values.title).indexOf(conv) != -1) {
+      this.selected_conversation = conv;
+      $("#curr_conv").text(conv.slice(1));
+    } else if (this.contacts.indexOf(conv) != -1) {
+      this.selected_conversation = conv;
+      $("#curr_conv").text("Chat with " + conv);
+    } else {
+      this.selected_conversation = null;
     }
+  }
 
-    get_active_contacts() {
-        $.getJSON("/users", contacts => {
-            this.contacts = contacts;
-        });
-        setTimeout(this.get_active_contacts, 1000);
-    }
+  tick() {
+    $.getJSON("/events", result => {
+      this.render();
+    });
+    this.render();
 
-    get_conversations() {
-        $.getJSON("/conversations", convs => {
-            this.conversations = convs;
-        });
-    }
-
-    render_conversations() { 
-        this.conversations_bar.render(this.conversations.map(values => values.title)); 
-    } 
-    render_pms() { this.pm_bar.render(this.contacts); } 
-    render_users_search() { users_search.render(this.contacts); }
-
-    render() {
-        this.render_conversations();
-        this.render_pms();
-        this.render_users_search();
-    }
-
-    select_conv(conv) {
-        if (this.conversations.map(values => values.title).indexOf(conv) != -1) {
-            this.selected_conversation = conv;
-            $("#curr_conv").text(conv.slice(1));
-        } else if (this.contacts.indexOf(conv) != -1) {
-            this.selected_conversation = conv;
-            $("#curr_conv").text("Chat with " + conv);
-        } else {
-            this.selected_conversation = null;
-        }
-    }
-
-    tick() {
-        $.getJSON("/events", result => {
-            this.render();
-        });
-        this.render();
-
-        setTimeout(this.tick.bind(this), 1000);
-    }
+    setTimeout(this.tick.bind(this), 1000);
+  }
 }
-
 
 var messenger = new Messenger();
 
+// Onload ====================================================
 $(document).ready(function() {
     messenger.tick();
 });
 
+// Constant ====================================================
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 })
